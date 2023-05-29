e3117da30dd4b326d4c24c1439d71d4f87874ba9
Denys Odnosum <dentyta5000@gmail.com>
2023-05-29 11:09:01 +0300

    there are no ideas for an optional task

---
+++ homework_js/hw0(intro)/script.js
@@ -0,0 +1,20 @@
+//Calc
+
+// {
+//     let name = prompt('Введіть ваше ім\'я');
+//     alert(`Hello, ${name}`);
+//     let age = prompt('Введіть ваш вік!');
+//     alert(`Через 100 років, Вам виповниться ${age+100}`)
+// }
+
+//Завдання на синій пояс
+
+{
+    const credentials = {
+        login: 'admin',
+        password: 'qwerty',
+    }
+    const div = document.createElement('div');
+    const input = document.createElement('input');
+    //не знаю, як з'єднати форму з перевіркою в js... 
+}
