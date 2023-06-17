49c80afe444cc6f9c76b659479f9d4b059432791
Denys Odnosum <dentyta5000@gmail.com>
2023-06-17 14:02:38 +0300

    added hw0(login and password)

--- homework_js/hw0(intro)/script.js
+++ homework_js/hw0(intro)/script.js
@@ -9,7 +9,7 @@
 
 //Завдання на синій пояс
 
-{
+/*{
     const credentials = {
         login: 'admin',
         password: 'qwerty',
@@ -17,4 +17,21 @@
     const div = document.createElement('div');
     const input = document.createElement('input');
     //не знаю, як з'єднати форму з перевіркою в js... 
+}*/
+{
+    let login = prompt('Введіть ваш логін:');
+    let password;
+    if(login == 'admin'){
+        alert('Вітаю, admin!');
+        password = prompt('Введіть пароль: ')
+        if(password=='qwerty'){
+            alert('Ви увійшли до системи:')
+        }
+        else{
+            alert('Пароль невірний, доступ заблоковано. Спробуйте зайти через 60 хвилин!')
+        }
+    }
+    else{
+        alert('Логін або пароль невірні!');
+    }
 }
